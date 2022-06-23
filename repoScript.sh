5499d6741d6232771523acd135c21d52a96a87df
ez98 <pi@raspberry>
2022-06-23 10:52:35 -0500

    push new table

---
+++ repoScript.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+JSON="/home/pi/Desktop/jq-test/repo.json"
+
+FORMAT=`cat "$JSON" | jq '[.[] | {Name: .commit.author.name, Message: .commit.message, Compare_Log: .html_url}]'`
+
+echo $FORMAT | jq '.'
+LENGTH=`echo "$FORMAT" | jq length`
+echo "| Name | Message | Changes |" >> /home/pi/Desktop/jq-test/README.md
+echo "| ---- | ----------- | ---- |" >> /home/pi/Desktop/jq-test/README.md
+str=""
+
+
+for(( i=0; i<$LENGTH; i++ ))
+do
+    str+="| "
+    str+=`echo $FORMAT | jq -r ".[$i].Name"`
+    str+=" | "
+    str+=`echo $FORMAT | jq -r ".[$i].Message"`
+    str+=" | "
+    str+=`echo $FORMAT | jq ".[$i].Compare_Log"`
+    str+=" |"
+    echo $str >> /home/pi/Desktop/jq-test/README.md
+    str=""
+done
+
+git add -A
+git commit -m "push new table"
+git push
+
