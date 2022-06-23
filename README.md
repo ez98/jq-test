ea1ccb5c11dc453cd11331e4f660851e6edbde49
ez98 <pi@raspberry>
2022-06-22 22:36:18 -0500

    test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 | Roll | Name | Batch | Department |
 | --- | --- | --- | --- |
-| test | test | test | test |
+| 3 | "Micheal" | 29 | "CSE" |
+| 55 | "Lisa" | 34 | "BBA" |
+| 12 | "John" | 22 | "English" |
--- script.sh
+++ script.sh
@@ -10,7 +10,31 @@ README="/home/pi/Desktop/jq-test/README.md"
 
 echo "| Roll | Name | Batch | Department |" >> "$README"
 echo "| --- | --- | --- | --- |" >> "$README"
-echo "| test | test | test | test |" >> "$README"
+#echo "| test | test | test | test |" >> "$README"
+
+LENGTH=`jq length "$JSONFILE"`
+#jq '.[0].roll' "$JSONFILE"
+
+str=""
+for (( i=0; i<$LENGTH; i++ ))
+
+do
+    echo "i = ${i}"
+    str+="| "    
+    str+=`jq ".[$i].roll" "$JSONFILE"`
+    str+=" | "
+    str+=`jq ".[$i].name" "$JSONFILE"`
+    str+=" | "
+    str+=`jq ".[$i].batch" "$JSONFILE"`
+    str+=" | "
+    str+=`jq ".[$i].department" "$JSONFILE"`
+    str+=" |"
+    echo "At end ${i}"
+    echo $str >> "$README"
+    str=""
+done
+
+
 git add -A
 git commit -m "test"
 git push
